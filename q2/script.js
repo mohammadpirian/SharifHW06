8da1f869b6a0ebcb609e18287039786c4ca48bf8
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-01-22 13:58:48 +0330

    add DOM

---
+++ q2/script.js
@@ -0,0 +1,24 @@
+// const increaseBtn = document.getElementById("increase");
+// const decreaseBtn = document.getElementById("decrease");
+// const resetBtn = document.getElementById("reset");
+// const readBtn = document.getElementById("read");
+
+// let stepCounter = {
+//   counter: 0,
+// };
+// function increase(counter) {
+//   counter = ++counter;
+// }
+// function decrease(counter) {
+//   counter = --counter;
+// }
+// function reset(counter) {
+//   counter = 0;
+// }
+// function read(counter) {}
+// increaseBtn.addEventListener("click", increase(stepCounter.counter));
+// decreaseBtn.addEventListener("click", decrease(stepCounter.counter));
+// resetBtn.addEventListener("click", reset(stepCounter.counter));
+// readBtn.addEventListener("click", read(){
+
+// });
