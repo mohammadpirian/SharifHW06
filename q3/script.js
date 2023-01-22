082cce73709a4ccea367115217947cb355b4dcf6
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-01-22 14:07:56 +0330

    creat listMember

---
+++ q3/script.js
@@ -0,0 +1,30 @@
+let listMember = [
+    {
+      name: "mohammad",
+      age: 25,
+    },
+    {
+      name: "ali",
+      age: 26,
+    },
+    {
+      name: "ahmad",
+      age: 71,
+    },
+    {
+      name: "reza",
+      age: 12,
+    },
+    {
+      name: "mohtashami",
+      age: 2,
+    },
+    {
+      name: "ehsan",
+      age: 69,
+    },
+    {
+      name: "saeed",
+      age: 40,
+    },
+  ];
