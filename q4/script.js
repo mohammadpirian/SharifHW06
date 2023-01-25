de5c62bad6f7876ff990c9833e4c7c806a23613a
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-01-25 21:43:18 +0330

    complete q4

--- q4/script.js
+++ q4/script.js
@@ -1,6 +1,16 @@
-function getData(){
-    top=prompt('please insert top')
-    right=prompt('please insert top')
-    top=prompt('please insert top')
-    top=prompt('please insert top')
+"use strict";
+const massage = document.querySelector(".massage");
+function helloworld(item) {
+  const styletop = item.top;
+  massage.style.top = `${styletop}px`;
+  massage.style.right = `${styletop}px`;
+  massage.innerHTML = item.html;
+  massage.classList.add = item.className;
+  console.log(massage.classList);
 }
+helloworld({
+  top: 500,
+  right: 500,
+  html: "Hello",
+  className: "welcome",
+});
